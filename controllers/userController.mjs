9f17dd8a6a1496a21158f591937cf653a19ea3d7
AlexBlacksmith <unbloody.alex@gmail.com>
2022-08-17 20:26:29 +0500

    Implemented JWT tokens generation

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -1,19 +1,59 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import { User } from '../models/user.mjs';
+import { hashData, generateJWT } from './helpers.mjs';
 
 export const registerUser = async (req, res) => {
     try {
-        const user = new User({...req.body});
-        await user.save();
+        const { name, login, email, password } = req.body;
+        const hashedPassword = await hashData(password);
+
+        const user = await User.create({
+            name,
+            login,
+            email,
+            password: hashedPassword   
+        });
+        if(!user) {
+            res.status(400).send({ message: "Error" });
+        }
         res
             .status(201)
-            .json({...user._doc});
+            .json({
+                id: user._id,
+                name: user.name,
+                login: user.login,
+                email: user.email,
+                token: generateJWT(user._id)
+            });
     } catch (err) {
         console.error(err)
-        res.status(400).end();
+        res.status(400).send({ message: "Error" });
     }
 }
 
-export const loginUser = (req, res) => {
-    res.json({ message: "login" });
+export const loginUser = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if(user && (await bcrypt.compare(password, user.password))) {
+            res.status(200).send({
+                id: user._id,
+                name: user.name,
+                login: user.login,
+                email: user.email,
+                token: generateJWT(user._id)
+            });
+        } else {
+            res.status(400).send({ message: "Invalid credentials" });
+        }
+    } catch(err) {
+        console.error(err)
+        res.status(400).send({ message: "Error" });
+    }
 }
 
+export const testAuth = async (req, res) => {
+    console.log(req.user);
+    res.status(200).json({ ...req.user });
+}
--- routes/userRouter.mjs
+++ routes/userRouter.mjs
@@ -1,10 +1,12 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/userController.mjs';
+import { registerUser, loginUser, testAuth } from '../controllers/userController.mjs';
+import { defender } from '../middleware/auth.mjs';
 
 const router = express.Router();
 
 router
     .post('/api/user/create', registerUser)
-    .post('/api/user/login', loginUser);
+    .post('/api/user/login', loginUser)
+    .get('/api/user/test', defender, testAuth);
 
 export default router;
